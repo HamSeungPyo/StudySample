544b8bcd28a66e6a5cadd9eb6da579f5bad1e66c
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-03-22 18:53:59 +0900

    플레이어 제작 및 벽돌
    플레이어 기본 움직임 공격 완료
    적 미완성 어떤식으로 할지 고민중

---
+++ Assets/Scripts/CameraFollowsPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowsPlayer : MonoBehaviour
+{
+    public GameObject player;
+    float speed = 3;
+    private void Start()
+    {
+        StartCoroutine(FollowPlayer());
+    }
+    IEnumerator FollowPlayer()
+    {
+        float targetPosX= player.transform.position.x;
+        while (true)
+        {
+            targetPosX = Mathf.Clamp(player.transform.position.x, -4, 100);
+
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetPosX, Time.deltaTime * speed), 0, -10);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    readonly float speed = 5;
+    public GameObject attack;
+    SpriteRenderer render;
+    Rigidbody2D rigid;
+    Animator anim;
+    bool bTreadOnGround = true;
+    float move = 0;
+    bool bAttackMode =false;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        render = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& bAttackMode)
+        {
+            anim.SetTrigger("Attack");
+        }
+        int animNum = 1;
+        move = Input.GetAxis("Horizontal");
+        if (move > 0)
+        {
+            attack.transform.localPosition = new Vector3(1.2f, attack.transform.localPosition.y, 0);
+            render.flipX = true;
+        }
+        else if (move < 0)
+        {
+            attack.transform.localPosition = new Vector3(-1.2f, attack.transform.localPosition.y, 0);
+            render.flipX = false;
+        }
+        else
+        {
+            animNum = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)&& bTreadOnGround)
+        {
+            animNum = 2;
+            rigid.velocity += new Vector2(0, 15);
+            bTreadOnGround = false;
+        }
+        else
+        {
+            RaycastHit2D hit;
+            if ((hit = Physics2D.Raycast(transform.position, -transform.up, 0.1f)) && rigid.velocity.y == 0)
+            {
+                if (hit.transform.tag == "Ground")
+                {
+                    bTreadOnGround = true;
+                }
+                else
+                {
+                    animNum = 2;
+                }
+                //Debug.Log(hit.collider.tag);
+                //Debug.DrawRay(transform.position, -transform.up*0.1f, Color.red, 1);
+            }
+            else
+            {
+                animNum = 2;
+            }
+        }
+        anim.SetInteger("Behavior", animNum);
+        transform.Translate(new Vector3(move, 0, 0) * Time.deltaTime * speed);
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Item")
+        {
+            bAttackMode = true;
+            Destroy(col.gameObject);
+            anim.SetBool("bAttackMode", bAttackMode);
+            anim.SetTrigger("ModeChange");
+        }
+    }
+}
